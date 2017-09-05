e16f024985857dd2f2fdc0229afc75404493a96d
reinvdwoerd <reinvanderwoerd@me.com>
2017-09-05 09:40:02 +0200

    topic_of_today

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,10 +2,12 @@ class Topic < ApplicationRecord
   has_many :artworks
 
   def self.of_today
-    if topic_of_today = Topic.where(date: Date.today)
+    if topic_of_today = Topic.where(date: Date.today).first
       topic_of_today
     else
-      Topic.where(date: nil).sample.update(date: Date.today)
+      topic_of_today = Topic.where(date: nil).sample
+      topic_of_today.update(date: Date.today)
+      topic_of_today
     end
   end
 end
